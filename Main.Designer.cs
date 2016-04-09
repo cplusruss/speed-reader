6ad382a5b8c3238e311405c99a55f1e2b627b83f
cplusruss <cplusruss@gmail.com>
2016-04-08 17:01:31 -0700

    Added files via upload

---
+++ Main.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SpeedReader
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTextView = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblWPM = new System.Windows.Forms.Label();
+            this.numClusterSize = new System.Windows.Forms.NumericUpDown();
+            this.numWPM = new System.Windows.Forms.NumericUpDown();
+            this.chkPlayPause = new System.Windows.Forms.CheckBox();
+            this.cmdOpenFile = new System.Windows.Forms.Button();
+            this.panel = new System.Windows.Forms.Panel();
+            this.trbLocation = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.numClusterSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWPM)).BeginInit();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbLocation)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTextView
+            // 
+            this.lblTextView.BackColor = System.Drawing.Color.White;
+            this.lblTextView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTextView.Font = new System.Drawing.Font("Calibri Light", 111.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTextView.ForeColor = System.Drawing.Color.Black;
+            this.lblTextView.Location = new System.Drawing.Point(0, 0);
+            this.lblTextView.Name = "lblTextView";
+            this.lblTextView.Size = new System.Drawing.Size(1005, 627);
+            this.lblTextView.TabIndex = 0;
+            this.lblTextView.Text = "Drag text or files ...";
+            this.lblTextView.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(212, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ClusterSize";
+            // 
+            // lblWPM
+            // 
+            this.lblWPM.AutoSize = true;
+            this.lblWPM.BackColor = System.Drawing.SystemColors.Control;
+            this.lblWPM.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblWPM.Location = new System.Drawing.Point(102, 11);
+            this.lblWPM.Name = "lblWPM";
+            this.lblWPM.Size = new System.Drawing.Size(34, 13);
+            this.lblWPM.TabIndex = 4;
+            this.lblWPM.Text = "WPM";
+            // 
+            // numClusterSize
+            // 
+            this.numClusterSize.Location = new System.Drawing.Point(277, 5);
+            this.numClusterSize.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numClusterSize.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numClusterSize.Name = "numClusterSize";
+            this.numClusterSize.Size = new System.Drawing.Size(49, 20);
+            this.numClusterSize.TabIndex = 3;
+            this.numClusterSize.Value = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numClusterSize.ValueChanged += new System.EventHandler(this.numClusterSize_ValueChanged);
+            // 
+            // numWPM
+            // 
+            this.numWPM.Location = new System.Drawing.Point(148, 6);
+            this.numWPM.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numWPM.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numWPM.Name = "numWPM";
+            this.numWPM.Size = new System.Drawing.Size(56, 20);
+            this.numWPM.TabIndex = 3;
+            this.numWPM.Value = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numWPM.ValueChanged += new System.EventHandler(this.numWPM_ValueChanged);
+            // 
+            // chkPlayPause
+            // 
+            this.chkPlayPause.Appearance = System.Windows.Forms.Appearance.Button;
+            this.chkPlayPause.AutoSize = true;
+            this.chkPlayPause.BackColor = System.Drawing.SystemColors.Control;
+            this.chkPlayPause.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkPlayPause.ForeColor = System.Drawing.Color.Black;
+            this.chkPlayPause.Location = new System.Drawing.Point(55, 3);
+            this.chkPlayPause.Name = "chkPlayPause";
+            this.chkPlayPause.Size = new System.Drawing.Size(41, 23);
+            this.chkPlayPause.TabIndex = 2;
+            this.chkPlayPause.Text = "Play";
+            this.chkPlayPause.UseVisualStyleBackColor = false;
+            this.chkPlayPause.CheckedChanged += new System.EventHandler(this.chkPlayPause_CheckedChanged);
+            // 
+            // cmdOpenFile
+            // 
+            this.cmdOpenFile.BackColor = System.Drawing.SystemColors.Control;
+            this.cmdOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdOpenFile.ForeColor = System.Drawing.Color.Black;
+            this.cmdOpenFile.Location = new System.Drawing.Point(3, 3);
+            this.cmdOpenFile.Name = "cmdOpenFile";
+            this.cmdOpenFile.Size = new System.Drawing.Size(46, 23);
+            this.cmdOpenFile.TabIndex = 0;
+            this.cmdOpenFile.Text = "Open";
+            this.cmdOpenFile.UseVisualStyleBackColor = false;
+            this.cmdOpenFile.Click += new System.EventHandler(this.cmdOpenFile_Click);
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.BackColor = System.Drawing.SystemColors.Control;
+            this.panel.Controls.Add(this.trbLocation);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Controls.Add(this.cmdOpenFile);
+            this.panel.Controls.Add(this.lblWPM);
+            this.panel.Controls.Add(this.chkPlayPause);
+            this.panel.Controls.Add(this.numClusterSize);
+            this.panel.Controls.Add(this.numWPM);
+            this.panel.Location = new System.Drawing.Point(-1, 596);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(1006, 31);
+            this.panel.TabIndex = 3;
+            // 
+            // trbLocation
+            // 
+            this.trbLocation.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.trbLocation.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trbLocation.LargeChange = 1;
+            this.trbLocation.Location = new System.Drawing.Point(332, 2);
+            this.trbLocation.Name = "trbLocation";
+            this.trbLocation.Size = new System.Drawing.Size(671, 42);
+            this.trbLocation.TabIndex = 5;
+            this.trbLocation.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trbLocation.Scroll += new System.EventHandler(this.trbLocation_Scroll);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1005, 627);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.lblTextView);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Main";
+            this.Text = "Speed Reader";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numClusterSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numWPM)).EndInit();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trbLocation)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTextView;
+        private System.Windows.Forms.Button cmdOpenFile;
+        private System.Windows.Forms.CheckBox chkPlayPause;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblWPM;
+        private System.Windows.Forms.NumericUpDown numClusterSize;
+        private System.Windows.Forms.NumericUpDown numWPM;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.TrackBar trbLocation;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,386 @@
+﻿//Features to implement
+//
+//Python version
+// Dragged text
+// HTML parsing
+// PDF support
+// Gutenberg + library link
+// EPUB support
+// Images ??? 
+// Keyboard commands 
+//     o          - Open file
+//     space      - Play/pause
+//     left/right - prev/next cluster
+//     s          - Split content/view
+// Dyanmically modulating WPM and chunk size (training) 
+// Keep the FOV the same, regardless of DPI
+// DOM support? i.e. parse the DOM of sites like Wikipedia
+// Learning ?
+// Scrolling "zooms into entire text" at smaller font but with highlight
+// Stop at end with reading statistics
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SpeedReader
+{
+    public partial class Main : Form
+    {
+        private string[] words;
+        private int clusterSize;
+        private int startWord;
+        private int endWord;
+
+        Timer readingTimer = new Timer();
+
+        public Main()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Main_DragEnter);
+            this.DragDrop += new DragEventHandler(Main_DragDrop);
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            trbLocation.Enabled = false;
+            chkPlayPause.Enabled = false;
+
+            numClusterSize.Value = 1;
+            numWPM.Value = 250;
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            readingTimer.Tick += new EventHandler(readingTimer_Tick);
+
+            KeyPreview = true;
+            KeyPress += new KeyPressEventHandler(Main_KeyPress);
+        }
+
+        void Main_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Space))
+            {
+                chkPlayPause.Checked = !chkPlayPause.Checked;
+                chkPlayPause_CheckedChanged(this, null);
+            }
+
+            switch (e.KeyChar.ToString().ToUpper())
+            {
+                case "O":
+                    cmdOpenFile_Click(this, null);
+                    break;
+                default:
+                    break;
+            }
+
+            e.Handled = true;
+            return;
+        }
+
+        void Main_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        void Main_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.StringFormat))
+            {
+                string dropText = (string)e.Data.GetData(DataFormats.StringFormat);
+                words = dropText.Split(' ', '\r', '\n');
+
+                SetupText();
+            }
+            else if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] draggedFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (draggedFiles.Length > 0)
+                {
+                    LoadFile(draggedFiles[0]);
+                }
+            }
+            else
+            {
+
+            }
+        }
+
+        private void cmdOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                string filePath = dialog.FileName;
+
+                if (filePath != null)
+                {
+                    LoadFile(filePath);
+                }
+            }
+            lblTextView.Select();
+        }
+
+        private string[] ReadFile(string filePath)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //Flatten the text
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                if (sb.Length > 0)
+                {
+                    sb.Append(" ");
+                }
+
+                sb.Append(line);
+            }
+            string flattened = sb.ToString();
+            return flattened.Split(' ', '\r', '\n');
+        }
+
+        private void LoadFile(string filePath)
+        {
+            words = ReadFile(filePath);
+            if (words.Length > 0)
+            {
+                SetupText();
+            }
+        }
+
+        public string GetReadableTimeSpan(TimeSpan value)
+        {
+            string duration = "";
+
+            var totalDays = (int)value.TotalDays;
+            if (totalDays >= 1)
+            {
+                duration = totalDays + " day" + (totalDays > 1 ? "s" : string.Empty);
+                value = value.Add(TimeSpan.FromDays(-1 * totalDays));
+            }
+
+            var totalHours = (int)value.TotalHours;
+            if (totalHours >= 1)
+            {
+                if (totalDays >= 1)
+                {
+                    duration += ", ";
+                }
+                duration += totalHours + " hour" + (totalHours > 1 ? "s" : string.Empty);
+                value = value.Add(TimeSpan.FromHours(-1 * totalHours));
+            }
+
+            var totalMinutes = (int)value.TotalHours;
+            if (totalMinutes >= 1)
+            {
+                if (totalHours >= 1)
+                {
+                    duration += ", ";
+                }
+                duration += totalMinutes + " minute" + (totalMinutes > 1 ? "s" : string.Empty);
+            }
+
+            return duration;
+        }
+ 
+        private void SetupText()
+        {
+            startWord = 0;
+            endWord = startWord + clusterSize;
+            trbLocation.Minimum = 0;
+            trbLocation.Maximum = (int)(words.Length / clusterSize);
+            UpdateText(GetNextWords(WordDirection.Forward));
+            trbLocation.Enabled = true;
+            chkPlayPause.Enabled = true;
+
+            //Compute reading time
+            string readingTimeStr = "";
+
+            double sec = (double)(words.Length / numWPM.Value) * 60;
+            TimeSpan Duration = TimeSpan.FromSeconds(sec);
+            DateTime EndTime = DateTime.Now.Add(Duration);
+
+            UpdateText("Read " + words.Length.ToString("#,##0") + " words\r\n" + " in " +
+                 GetReadableTimeSpan(Duration) +
+                 "\r\n" + " by " + EndTime.ToString("hh:mm:ss"));
+ 
+            //Remove noise
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+
+                if (words[i].Equals("A")) words[i] = "";
+                if (words[i].Equals("AN")) words[i] = "";
+                if (words[i].Equals("AND")) words[i] = "";
+                if (words[i].Equals("THE")) words[i] = "";
+                if (words[i].Equals("OF")) words[i] = ""; 
+                if (words[i].Equals("THIS")) words[i] = "";
+                if (words[i].Equals("ITS")) words[i] = "";
+                if (words[i].Equals("THEN")) words[i] = "";
+                if (words[i].Equals("FURTHER")) words[i] = "";
+                if (words[i].Equals("FURTHERMORE")) words[i] = "";
+                if (words[i].Equals("ARE")) words[i] = "";
+                if (words[i].Equals("HAS")) words[i] = "";
+                if (words[i].Equals("AS")) words[i] = "";
+                if (words[i].Equals("BE")) words[i] = "";
+                if (words[i].Equals("CAN")) words[i] = "";
+                if (words[i].Equals("TO")) words[i] = "";
+                if (words[i].Equals("FOR")) words[i] = "";
+                if (words[i].Equals("BY")) words[i] = "";
+                if (words[i].Equals("WITH")) words[i] = "";
+                if (words[i].Equals("IS")) words[i] = "";
+                if (words[i].Equals("SO")) words[i] = "";
+                if (words[i].Equals("THAT")) words[i] = "";
+                if (words[i].Equals("IN")) words[i] = "";
+                if (words[i].Equals("IT")) words[i] = "";
+
+                words[i] = words[i].Replace(";", "");
+                words[i] = words[i].Replace("-", "");
+                words[i] = words[i].Replace("—", "");
+                words[i] = words[i].Replace(",", "");
+                words[i] = words[i].Replace(".", "");
+                words[i] = words[i].Replace("\"", "");
+                words[i] = words[i].Replace(":", "");
+                words[i] = words[i].Replace(".", "");
+                words[i] = words[i].Replace("*", "");
+            }
+        }
+
+        private void chkPlayPause_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkPlayPause.Checked)
+            {
+                chkPlayPause.Text = "Pause";
+
+                numWPM_ValueChanged(this, null);
+                readingTimer.Start();
+            }
+            else
+            {
+                chkPlayPause.Text = "Play";
+                readingTimer.Stop();
+            
+            }
+            lblTextView.Select();
+        }
+
+        /// <summary>
+        /// Handle the playback of text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void readingTimer_Tick(object sender, EventArgs e)
+        {
+            UpdateText(GetNextWords(WordDirection.Forward));
+            trbLocation.Value = startWord / clusterSize;
+        }
+
+        /// <summary>
+        /// Get words in direction
+        /// </summary>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+        enum WordDirection
+        {
+            Forward,
+            Reverse
+        }
+        private string GetNextWords(WordDirection direction)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (direction == WordDirection.Forward)
+            {
+                startWord += clusterSize;
+                if ((startWord + clusterSize) > words.Length)
+                {
+                    startWord = 0;
+                }
+            }
+            else if (direction == WordDirection.Reverse)
+            {
+                startWord -= clusterSize;
+                if (startWord < 0)
+                {
+                    startWord = words.Length - clusterSize;
+                }
+            }
+            endWord = (startWord + clusterSize);
+            for (int word = startWord; word < endWord; word++)
+            {
+                sb.Append(words[word]);
+                sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+
+        private string GetWords(int location)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            startWord = location;
+            endWord = startWord + clusterSize;
+
+            if (endWord > words.Length)
+            {
+                endWord = 0;
+            }
+
+            for (int word = startWord; word < endWord; word++)
+            {
+                sb.Append(words[word]);
+                sb.Append(" ");
+            }
+
+            return sb.ToString();
+        }
+
+        delegate void UpdateTextCallback(string text);
+        void UpdateText(string text)
+        {
+            if (this.lblTextView.InvokeRequired)
+            {
+                UpdateTextCallback d = new UpdateTextCallback(UpdateText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.lblTextView.Text = text.ToUpper();
+            }
+        }
+
+        private void numClusterSize_ValueChanged(object sender, EventArgs e)
+        {
+            clusterSize = (int)numClusterSize.Value;
+        }
+
+        private void numWPM_ValueChanged(object sender, EventArgs e)
+        {
+            int wpm = (int)numWPM.Value;
+            readingTimer.Interval = (int)(60000 / (wpm / clusterSize));
+        }
+ 
+        private void trbLocation_Scroll(object sender, EventArgs e)
+        {
+            UpdateText(GetWords(trbLocation.Value * clusterSize));
+        }
+    }
+}
